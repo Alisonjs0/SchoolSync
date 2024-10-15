5aaccbcd760e698a954fe4d82fc7e8ba3e41b684
Alison <alisonserafim.tec@gmail.com>
2024-10-15 15:59:10 -0300

    Inicio do template do dashboard

---
+++ app/dashboard_main/page.js
@@ -0,0 +1,24 @@
+import "./dashboardTemplate.css"
+
+const dashboard_main = () => {
+    return (
+        <div className="page">
+            <div className="menuLateral">
+                
+            </div>
+            <div className="menuSuperior">
+
+            </div>
+            <div className="conteudo">
+                <h3>Dashboard</h3>
+                <div className={`${"container"} ${"conteudoMenus"}`}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default dashboard_main
