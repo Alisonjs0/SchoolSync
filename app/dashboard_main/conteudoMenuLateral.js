fd78bce0d888e5c6eb7e64f2ab2f3c788c29b511
Alison <alisonserafim.tec@gmail.com>
2024-10-20 12:51:31 -0300

    ajustes no esqueleto do dashboard

--- app/dashboard_main/conteudoMenuLateral.js
+++ app/dashboard_main/conteudoMenuLateral.js
@@ -4,8 +4,9 @@ import { MdDashboard } from "react-icons/md";
 import { IoCalendar } from "react-icons/io5";
 import { IoPeople } from "react-icons/io5";
 import { FaComments } from "react-icons/fa";
-import { FaTools } from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
 import { FaUserPlus } from "react-icons/fa6";
+import { FaClock } from "react-icons/fa";
 
 const ConteudoMenuLateral = () => {
 
@@ -23,20 +24,27 @@ const ConteudoMenuLateral = () => {
                 <ul className="listaLateral">
                     {user.tipo === "professor" ? (
                         <div>
-                            <li><MdDashboard className="iconMenuLateral" /> <p>Dashboard</p></li>
-                            <li><IoCalendar className="iconMenuLateral" /> <p>Calendario</p></li>
-                            <li><IoPeople className="iconMenuLateral" /> <p>Gestão de turmas</p></li>
-                            <li><FaComments className="iconMenuLateral" /> <p>Contato e suporte</p></li>
-                            <li><FaTools className="iconMenuLateral" /> <p>Configurações</p></li>
+                            <li><a href=""><MdDashboard className="iconMenuLateral" /> <p>Dashboard</p></a></li>
+                            <li><a href=""><IoCalendar className="iconMenuLateral" /> <p>Calendario Escolar</p></a></li>
+                            <li><a href=""><IoPeople className="iconMenuLateral" /> <p>Gestão de turmas</p></a></li>
+                            <li><a href=""><FaComments className="iconMenuLateral" /> <p>Contato e suporte</p></a></li>
+                            <li><a href=""><FaTools className="iconMenuLateral" /> <p>Configurações</p></a></li>
                         </div>
                     ) : user.tipo === "admin" ? (
                         <div>
-                            <li><MdDashboard className="iconMenuLateral" /> <p>Dashboard</p></li>
-                            <li><IoCalendar className="iconMenuLateral" /> <p>Calendario</p></li>
-                            <li><FaUserPlus className="iconMenuLateral"/> <p>Cadastrar Usuario</p></li>
-                            <li><IoPeople className="iconMenuLateral" /> <p>Gestão de turmas</p></li>
-                            <li><FaComments className="iconMenuLateral" /> <p>Contato e suporte</p></li>
-                            <li><FaTools className="iconMenuLateral" /> <p>Configurações</p></li>
+                            <li><a href=""><MdDashboard className="iconMenuLateral" /> <p>Dashboard</p></a></li>
+                            <li><a href=""><IoCalendar className="iconMenuLateral" /> <p>Calendario Escolar</p></a></li>
+                            <li><a href=""><FaUserPlus className="iconMenuLateral"/> <p>Cadastrar Usuario</p></a></li>
+                            <li><a href=""><IoPeople className="iconMenuLateral" /> <p>Gestão de turmas</p></a></li>
+                            <li><a href=""><IoMdSettings className="iconMenuLateral" /> <p>Configurações</p></a></li>
+                        </div>
+                    ) : user.tipo === "aluno" ? (
+                        <div>
+                            <li><a href=""><MdDashboard className="iconMenuLateral" /> <p>Dashboard</p></a></li>
+                            <li><a href=""><IoCalendar className="iconMenuLateral" /> <p>Calendario Escolar</p></a></li>
+                            <li><a href=""><FaClock className="iconMenuLateral"/> <p>Horarios</p></a></li>
+                            <li><a href=""><FaComments className="iconMenuLateral" /> <p>Contato e suporte</p></a></li>
+                            <li><a href=""><FaComments className="iconMenuLateral" /> <p>Contato e suporte</p></a></li>
                         </div>
                     ) : null}
 
--- app/dashboard_main/conteudoMenus.js
+++ app/dashboard_main/conteudoMenus.js
@@ -1,41 +1,72 @@
 import './dashboardTemplate.css'
 
+import { IoAnalytics } from "react-icons/io5";
+
 const ConteudoMenus = () => {
 
     const user = {
         nome: 'Alison Jose Serafim de Lima',
         notas: [8,6,7,9],
         media: 7.5,
-        tipo: 'admin'
+        tipo: 'professor'
     }
 
   return (
             <div className={`${"container"} ${"conteudoMenus"}`}>
-                    <div className="menu1">
-                        <h2>
-                            Seja bem vindo, {user.nome.split(" ")[0]}
-                        </h2>
-                        <p>
-                            SchoolSync Dashboard
-                        </p>
-                    </div>
-                        {user.tipo === 'aluno' ? (
+                    {user.tipo === 'aluno' ? (
                             <div>
-                                <h2>aluno</h2>
-                                <p>Aluno</p>
+                                <a href="" className='menus'>
+                                    <h2>Seja bem vindo, {user.nome.split(" ")[0]} </h2>
+                                    <p>SchoolSync Dashboard.</p>
+                                </a>
+                                <a href="" className='menus'>
+                                    <h2>Calendario Escolar</h2> 
+                                    <p>Calendario da Instituição escolar.</p>
+                                </a>
+                                <a href="" className='menus'>
+                                    <h2>Horarios</h2>
+                                    <p>Seus horários escolares semanais.</p>
+                                </a>
+                                <a href="" className='menus'>
+                                    <h2>Boletim</h2>
+                                    <p>Vizualize suas notas.</p>
+                                </a>
                             </div>
                         ) : user.tipo === "admin" ? (
                             <div>
-                                <h2>Admin</h2>
-                                <p>admin</p>
+                                <a href="" className='menus'>
+                                    <h2>Seja bem vindo, {user.nome.split(" ")[0]} </h2>
+                                    <p>SchoolSync Dashboard.</p>
+                                </a>
+                                <a href="" className='menus  menuComicon'>
+                                    <h2>8.3</h2>
+                                    <p>Média Escolar</p>
+                                    <IoAnalytics className='menuIcon' />
+
+                                </a>
+                                <a href="" className='menus'>
+                                    <h2>Horarios</h2>
+                                    <p>Seus horários escolares semanais.</p>
+                                </a>
                             </div>
-                        ) : (
-                            <h1>ola</h1>
-                        )}
-                    
-                    <div>
-                        oi
-                    </div>
+                        ) : user.tipo === "professor" ? (
+                            <div>
+                                <a href="" className='menus'>
+                                    <h2>Seja bem vindo, {user.nome.split(" ")[0]} </h2>
+                                    <p>SchoolSync Dashboard.</p>
+                                </a>
+                                <a href="" className='menus  menuComicon'>
+                                    <h2>8.3<span>^13,3%</span></h2>
+                                    <p>Média por turma.</p>
+                                    <IoAnalytics className='menuIcon' />
+
+                                </a>
+                                <a href="" className='menus'>
+                                    <h2>Minhas Turmas</h2>
+                                    <p>Ranking e edição.</p>
+                                </a>
+                            </div>
+                        ) : null}
                 </div>
 
   )
