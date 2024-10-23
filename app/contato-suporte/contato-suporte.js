8e9e4bb86077e0e66039eb2f6b0cad721f95e291
ritta <ritavatteroni55555@gmail.com>
2024-10-23 18:18:44 -0300

    Pág ContatoSuporte em andamento

---
+++ app/contato-suporte/contato-suporte.js
@@ -0,0 +1,13 @@
+import EsqueletoPrincipal from '../esqueletoPrincipal/page.js'
+
+const ContatoSuporte = () => {
+    return (
+        <EsqueletoPrincipal>
+            <div className='container conteudoMenus'>
+                <h3>AAAAAAAAAAAA</h3>
+            </div>
+        </EsqueletoPrincipal>
+    )
+}
+
+export default ContatoSuporte
