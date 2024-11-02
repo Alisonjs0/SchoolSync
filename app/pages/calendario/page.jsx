4885548e6a57423d9d1163af4669ffd220e84db9
Alisonjs0 <alisonserafim.tec@gmail.com>
2024-11-02 09:59:56 -0300

    criacao e estilizacao do calendario

---
+++ app/pages/calendario/page.jsx
@@ -0,0 +1,31 @@
+/// INSTALAR PARA O CALENDARIO FUNCIONAR
+// npm install @fullcalendar/core @fullcalendar/daygrid @fullcalendar/interaction
+// npm install bootstrap
+// npm install uuid
+// npm install moment
+
+'use client'
+
+import dynamic from "next/dynamic";
+import EsqueletoPrincipal from "../esqueletoPrincipal/page.jsx";
+
+import "./calendario.css"
+
+const CalendarComponent = dynamic(() => import("./components/calendar.jsx"), {
+  ssr: false, // Desativa SSR para esse componente
+});
+
+export default function Page() {
+  return (
+    <div>
+      <EsqueletoPrincipal>
+        <div className="conteiner conteudoMenus">
+          <h3 className="titulo">Calendario Escolar</h3>
+          <div className="conteudo">
+            <CalendarComponent></CalendarComponent>
+          </div>
+        </div>
+      </EsqueletoPrincipal>
+    </div>
+  );
+}
