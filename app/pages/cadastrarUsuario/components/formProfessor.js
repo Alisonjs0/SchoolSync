3a3118e87e55b9da402cdb7beda4ac7ff034b669
Alisonjs0 <alisonserafim.tec@gmail.com>
2024-11-07 15:11:11 -0300

    inicio do forms professor

--- app/pages/cadastrarUsuario/components/formProfessor.js
+++ app/pages/cadastrarUsuario/components/formProfessor.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const formProfessor = () => {
   return (
     <div>
-      
+      <form>
+          <input type="text" placeholder="Area de Formacao"/>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default formProfessor
+export default formProfessor;
